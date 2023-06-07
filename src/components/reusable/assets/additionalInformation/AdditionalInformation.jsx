48b348934752c0ff615c7d1955a1be000c96037e
Alexandr Matviienko <matvienko@mettevo.org>
2023-06-07 16:51:02 +0300

    ref link

--- src/components/reusable/assets/additionalInformation/AdditionalInformation.jsx
+++ src/components/reusable/assets/additionalInformation/AdditionalInformation.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AdditionalInformationPlan from "./items/additionalInformationPlan/AdditionalInformationPlan.jsx";
 import HubWallet from "../../../pages/hub/hubMain/items/hubWallet/HubWallet.jsx";
 import Button from "../../btns/buttons/Button.jsx";
+import {Link} from "react-router-dom";
 
 const AdditionalInformation = ({onPresetBtn, onMyTaskBtn}) => {
     return (
@@ -10,7 +11,9 @@ const AdditionalInformation = ({onPresetBtn, onMyTaskBtn}) => {
             <HubWallet/>
             {onPresetBtn && <div className="additional-information__btn mt-3">
                 <Button className="button_primary w-100 button_m double-secondary px-6 px-lg-7">
+                  <Link to='/hub/add-preset'>
                     Add My Own Preset
+                  </Link>
                 </Button>
             </div>}
             {onMyTaskBtn && <div className="additional-information__btn mt-3">
