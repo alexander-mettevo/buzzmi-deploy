a3d2370c0bbba36747bafb7bd3edecc9a610d119
Pabli <pavel.vikhristyuk@fountainhead-it.com>
2023-06-08 14:44:41 +0300

    leaders page

--- src/components/reusable/board/Board.jsx
+++ src/components/reusable/board/Board.jsx
@@ -1,27 +1,31 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Board = ({items}) => {
     return items && items.length > 0 ? (
         <div className="board">
             {items.map(item => {
-                return (
-                    <div className={"board__item " + (item.itemBgColor ? item.itemBgColor : "")}>
-                        {item.img && <div className={"board__item-img " + (item.iconBgColor ? item.iconBgColor : "")}>
-                            <img src={item.img} alt={item.text}/>
+                const itemContent = <>
+                    {item.img && <div className={"board__item-img " + (item.iconBgColor ? item.iconBgColor : "")}>
+                        <img src={item.img} alt={item.text}/>
+                    </div>}
+                    <div className="board__item-holder">
+                        <span className="board__item-text">{item.text}</span>
+                        {item.imgList && item.imgList.length > 0 && <div className="board__item-additional">
+                            {item.imgList.map((img, index) => {
+                                return (
+                                    <div className="board__item-additional-img" key={index}>
+                                        <img src={img} alt="img"/>
+                                    </div>
+                                )
+                            })}
                         </div>}
-                        <div className="board__item-holder">
-                            <span className="board__item-text">{item.text}</span>
-                            {item.imgList && item.imgList.length > 0 && <div className="board__item-additional">
-                                {item.imgList.map((img, index) => {
-                                    return (
-                                        <div className="board__item-additional-img" key={index}>
-                                            <img src={img} alt="img"/>
-                                        </div>
-                                    )
-                                })}
-                            </div>}
-                        </div>
                     </div>
+                </>
+                return item.link ? (
+                    <Link to={item.link} className={"board__item " + (item.itemBgColor ? item.itemBgColor : "")} key={item.id}>{itemContent}</Link>
+                ) :  (
+                    <div className={"board__item " + (item.itemBgColor ? item.itemBgColor : "")} key={item.id}>{itemContent}</div>
                 )
             })}
 
