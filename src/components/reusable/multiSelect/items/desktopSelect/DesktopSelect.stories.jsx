f1aa32d506b795d829f3868a1cd472f88fc10c09
Alexandr Matviienko <matvienko@mettevo.org>
2023-06-14 21:35:19 +0300

    add mobile select

---
+++ src/components/reusable/multiSelect/items/desktopSelect/DesktopSelect.stories.jsx
@@ -0,0 +1,12 @@
+import DesktopSelect from "./DesktopSelect.jsx"
+
+export default {
+  title: "Reusable/MultiSelect/items/DesktopSelect/DesktopSelect",
+  component: DesktopSelect,
+  tags: ["autodocs"],
+  argTypes: {},
+}
+
+const Template = (args) => <DesktopSelect {...args} />
+export const Default = Template.bind({})
+Default.args = {}
