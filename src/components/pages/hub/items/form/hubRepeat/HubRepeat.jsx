63c3366a6d263bbf745f658a1b744d04008bcd82
Alexandr Matviienko <matvienko@mettevo.org>
2023-06-18 21:47:07 +0300

    create date block

--- src/components/pages/hub/items/form/hubRepeat/HubRepeat.jsx
+++ src/components/pages/hub/items/form/hubRepeat/HubRepeat.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react"
 import HubRepeatMode from "./items/hubRepeatMode/HubRepeatMode.jsx"
 import HubRepeatSwitcher from "./items/hubRepeatSwitcher/HubRepeatSwitcher.jsx"
+import HubRepeatEndDate from "./items/hubRepeatEndDate/HubRepeatEndDate.jsx"
 
 const HubRepeat = ({ values, setValues }) => {
   return (
     <>
       <HubRepeatMode values={values} setValues={setValues} />
       <HubRepeatSwitcher values={values} setValues={setValues} />
+      <HubRepeatEndDate values={values} setValues={setValues} />
     </>
   )
 }
--- src/components/pages/hub/items/form/hubRepeat/items/hubRepeatCalendar/HubRepeatCalendar.jsx
+++ src/components/pages/hub/items/form/hubRepeat/items/hubRepeatCalendar/HubRepeatCalendar.jsx
@@ -4,6 +4,8 @@ import Calendar from "../../../../../../../reusable/calendar/Calendar.jsx"
 const HubRepeatCalendar = () => {
   const [calendarValue, setCalendarValue] = useState(new Date())
 
+  //TODO организовать сохранение в values в основной стейт
+
   return <Calendar onChange={setCalendarValue} value={calendarValue} noIndent period="month" />
 }
 
