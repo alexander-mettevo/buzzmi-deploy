967a7b869eb36443b57130a3455b75c8df1330c2
Alexandr Matviienko <matvienko@mettevo.org>
2023-06-21 19:27:42 +0300

    ref slider for register page

--- src/components/layouts/doubleLayout/items/DoubleLayoutImage.jsx
+++ src/components/layouts/doubleLayout/items/DoubleLayoutImage.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import SupportBtn from "../../../reusable/btns/supportBtn/SupportBtn.jsx";
-import Tooltip from "../../../reusable/assets/tooltip/Tooltip.jsx";
-import {layoutData} from "./mocData.js";
-import Slider from "../../../reusable/slider/Slider.jsx";
+import React from "react"
+import SupportBtn from "../../../reusable/btns/supportBtn/SupportBtn.jsx"
+import Tooltip from "../../../reusable/assets/tooltip/Tooltip.jsx"
+import { layoutData } from "./mocData.js"
+import Slider from "../../../reusable/slider/Slider.jsx"
 
-const DoubleLayoutImage = ({reverse, imageUrl}) => {
+const DoubleLayoutImage = ({ reverse, imageUrl }) => {
   return (
-    <div className={`double-layout__intro ${imageUrl ? 'double-layout__intro_bg' : ''}`}>
-      <Tooltip position={!reverse ? 'left' : 'right'} tooltipText={'Any problems? Report Please  '}>
-        <SupportBtn/>
+    <div className={`double-layout__intro ${imageUrl ? "double-layout__intro_bg" : ""}`}>
+      <Tooltip position={!reverse ? "left" : "right"} tooltipText={"Any problems? Report Please  "}>
+        <SupportBtn />
       </Tooltip>
-      {
-        imageUrl ?
-          <div className='double-layout__bg-wrapper'>
-            <img src={imageUrl} alt="bg"/>
-          </div>
-          :
-          <Slider components={layoutData} interval={10000}/>
-      }
+      {imageUrl ? (
+        <div className="double-layout__bg-wrapper">
+          <img src={imageUrl} alt="bg" />
+        </div>
+      ) : (
+        <Slider components={layoutData} interval={5000} />
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default DoubleLayoutImage;
+export default DoubleLayoutImage
--- src/components/reusable/slider/Slider.jsx
+++ src/components/reusable/slider/Slider.jsx
@@ -1,30 +1,31 @@
-import React, {useState, useEffect} from "react";
-import DoubleLayoutItem from "../../layouts/doubleLayout/items/DoubleLayoutItem.jsx";
+import React, { useState, useEffect } from "react"
+import DoubleLayoutItem from "../../layouts/doubleLayout/items/DoubleLayoutItem.jsx"
 
-const Slider = ({components, interval}) => {
-  const [current, setCurrent] = useState(0);
+const Slider = ({ components, interval }) => {
+  const [current, setCurrent] = useState(0)
 
   useEffect(() => {
     const timeout = setInterval(() => {
-      setCurrent((current + 1) % components.length);
-    }, interval);
+      setCurrent((current + 1) % components.length)
+    }, interval)
 
-    return () => clearInterval(timeout);
-  }, [current, components, interval]);
+    return () => clearInterval(timeout)
+  }, [current, components, interval])
 
   return (
     <div className="slider">
-      {components.map((item, index) =>
-        <div
-          key={index}
-          className={`slide ${index === current ? "active" : ""}`}
-        >
-          <DoubleLayoutItem key={index + 'intro'} title={item?.title} description={item?.description}
-                            image={item?.image}/>
+      {components.map((item, index) => (
+        <div key={index} className={`slide ${index === current ? "active" : ""}`}>
+          <DoubleLayoutItem
+            key={index + "intro"}
+            title={item?.title}
+            description={item?.description}
+            image={item?.image}
+          />
         </div>
-      )}
+      ))}
     </div>
-  );
-};
+  )
+}
 
-export default Slider;
+export default Slider
