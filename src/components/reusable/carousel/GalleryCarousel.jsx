8a2033f0634d406f5bb40252b9cefa5000bd0f92
Alexandr Matviienko <matvienko@mettevo.org>
2023-06-26 12:28:32 +0300

    commit

--- src/components/reusable/carousel/GalleryCarousel.jsx
+++ src/components/reusable/carousel/GalleryCarousel.jsx
@@ -1,82 +1,84 @@
-import React, {useEffect, useRef, useState} from "react";
-import {Swiper, SwiperSlide} from "swiper/react";
-import {FreeMode, Navigation, Thumbs} from "swiper";
+import React, { useEffect, useRef, useState } from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { FreeMode, Navigation, Thumbs } from "swiper"
 
-const GalleryCarousel = ({list, overlineText, goToActiveSlide, onSwiperChange}) => {
-    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+const GalleryCarousel = ({ list, overlineText, goToActiveSlide, onSwiperChange }) => {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null)
 
-    const carouselNav = useRef(null);
+  const carouselNav = useRef(null)
 
-    const carouselPrevBtn = useRef(null);
-    const carouselNextBtn = useRef(null);
+  const carouselPrevBtn = useRef(null)
+  const carouselNextBtn = useRef(null)
 
-    useEffect(() => {
-        if (carouselNav && carouselNav.current) {
-            setThumbsSwiper(carouselNav.current)
-        }
-    }, [carouselNav])
+  useEffect(() => {
+    if (carouselNav && carouselNav.current) {
+      setThumbsSwiper(carouselNav.current)
+    }
+  }, [carouselNav])
 
-    return(
-        <div className="gallery__carousel">
-            {overlineText && <span className="gallery__carousel-overline">{overlineText}</span>}
-            {thumbsSwiper && <div className="gallery__carousel-body">
-                <Swiper
-                    initialSlide={goToActiveSlide}
-                    style={{
-                        "--swiper-navigation-color": "#fff",
-                        "--swiper-pagination-color": "#fff",
-                    }}
-                    navigation={{
-                        prevEl: carouselPrevBtn.current,
-                        nextEl: carouselNextBtn.current,
-                    }}
-                    spaceBetween={10}
-                    thumbs={{swiper: carouselNav.current}}
-                    modules={[FreeMode, Navigation, Thumbs]}
-                    className="mySwiper2"
-                    freeMode={true}
-                    onSlideChange={onSwiperChange}
-                    onSwiper={onSwiperChange}
-                >
-                    {list.map(el => {
-                        return (
-                            <SwiperSlide key={el.id}>
-                                <div className="gallery__carousel-item">
-                                    <img src={el.img} alt={el.description}/>
-                                </div>
-                            </SwiperSlide>
-                        )
-                    })}
-                </Swiper>
-                <button className="gallery__btn gallery__btn--prev" ref={carouselPrevBtn}>
-                    <img src="/images/assets/back.svg" alt="arrow icon"/>
-                </button>
-                <button className="gallery__btn gallery__btn--next" ref={carouselNextBtn}>
-                    <img src="/images/assets/back.svg" alt="arrow icon"/>
-                </button>
-            </div>}
-            <Swiper
-                ref={carouselNav}
-                initialSlide={goToActiveSlide}
-                onSwiper={setThumbsSwiper}
-                spaceBetween={0}
-                slidesPerView="auto"
-                watchSlidesProgress={true}
-                modules={[FreeMode, Navigation, Thumbs]}
-                className="mySwiper gallery__carousel-nav"
-            >
-                {list.map(el => {
-                    return (
-                        <SwiperSlide key={el.id}>
-                            <div className="gallery__item">
-                                <img src={el.img} alt={el.description}/>
-                            </div>
-                        </SwiperSlide>
-                    )
-                })}
-            </Swiper>
+  return (
+    <div className="gallery__carousel">
+      {overlineText && <span className="gallery__carousel-overline">{overlineText}</span>}
+      {thumbsSwiper && (
+        <div className="gallery__carousel-body">
+          <Swiper
+            initialSlide={goToActiveSlide}
+            style={{
+              "--swiper-navigation-color": "#fff",
+              "--swiper-pagination-color": "#fff",
+            }}
+            navigation={{
+              prevEl: carouselPrevBtn.current,
+              nextEl: carouselNextBtn.current,
+            }}
+            spaceBetween={10}
+            thumbs={{ swiper: carouselNav.current }}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="mySwiper2"
+            freeMode={true}
+            onSlideChange={onSwiperChange}
+            onSwiper={onSwiperChange}
+          >
+            {list.map((el) => {
+              return (
+                <SwiperSlide key={el.id}>
+                  <div className="gallery__carousel-item">
+                    <img src={el.img} alt={el.description} />
+                  </div>
+                </SwiperSlide>
+              )
+            })}
+          </Swiper>
+          <button className="gallery__btn gallery__btn--prev" ref={carouselPrevBtn}>
+            <img src="/images/assets/back.svg" alt="arrow icon" />
+          </button>
+          <button className="gallery__btn gallery__btn--next" ref={carouselNextBtn}>
+            <img src="/images/assets/back.svg" alt="arrow icon" />
+          </button>
         </div>
-    )
+      )}
+      <Swiper
+        ref={carouselNav}
+        initialSlide={goToActiveSlide}
+        onSwiper={setThumbsSwiper}
+        spaceBetween={0}
+        slidesPerView="auto"
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper gallery__carousel-nav"
+      >
+        {list.map((el) => {
+          return (
+            <SwiperSlide key={el.id}>
+              <div className="gallery__item">
+                <img src={el.img} alt={el.description} />
+              </div>
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </div>
+  )
 }
 
-export default GalleryCarousel;
+export default GalleryCarousel
