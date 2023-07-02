2dad0c60a5b54fb33aee938757a2fe0a7a96b466
Alexandr Matviienko <matvienko@mettevo.org>
2023-07-03 00:35:28 +0300

    ref date

--- src/components/reusable/form/items/inputs/dateInputs/DateInput.jsx
+++ src/components/reusable/form/items/inputs/dateInputs/DateInput.jsx
@@ -2,9 +2,11 @@ import { useState } from "react"
 import CalendarSvg from "./CalendarSVG.jsx"
 import cn from "classnames"
 import Calendar from "../../../../calendar/Calendar.jsx"
+import { Tooltip } from "react-tooltip"
+import moment from "moment"
 
 const DateInput = ({ label, setValue, errors }) => {
-  const [value, onChange] = useState(new Date())
+  const [value, onChange] = useState(null)
 
   const handleDateChange = (date) => {
     onChange(date)
@@ -12,31 +14,31 @@ const DateInput = ({ label, setValue, errors }) => {
   }
 
   return (
-    <div className="input-label">
+    <div className="input-label date-input">
       <div>{label}</div>
-      <div></div>
-      <div className="date-wrapper">
-        <Calendar
-          className={cn("date-picker date-picker_mode-switch", {
-            "line-wrapper": !value,
-            "_form-error": errors,
-          })}
-          onChange={handleDateChange}
-          value={value}
+      <div className="date-input__field-wrapper">
+        <input
+          type="text"
+          className="input input_simple-text date-input__field"
+          placeholder=""
+          value={value ? moment(value).format("MM/DD/YYYY") : "MM/DD/YYYY"}
         />
-        {/*  <DatePicker*/}
-        {/*    className={`date-picker ${!value ? 'line-wrapper' : ''} ${errors ? '_form-error' : ''}`}*/}
-        {/*    onChange={handleDateChange}*/}
-        {/*    value={value}*/}
-        {/*    calendarIcon={<Arrow/>}*/}
-        {/*    clearIcon={null}*/}
-        {/*    dayPlaceholder={'DD'}*/}
-        {/*    monthPlaceholder={'MM'}*/}
-        {/*    yearPlaceholder={'YYYY'}*/}
-        {/*    format={'dd/MM/yyyy'}*/}
-        {/*  />*/}
         <CalendarSvg />
       </div>
+      <Tooltip className="date-input__tooltip" clickable anchorSelect=".date-input__field" place="top">
+        <div className="date-input__wrapper">
+          <Calendar
+            className={cn("date-picker date-picker_mode-switch", {
+              "line-wrapper": !value,
+              "_form-error": errors,
+            })}
+            onChange={handleDateChange}
+            value={value || new Date()}
+            view=""
+          />
+        </div>
+      </Tooltip>
+
       <div>{errors && <div className="text-error text-center">{errors[0]}</div>}</div>
       <div className="date-wrapper__subtitle text-r">Your birthday won’t be shown publicly</div>
     </div>
