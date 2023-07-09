4988bd5afa7fc9964eca4b9fb0b74926f7412d7a
Alexandr Matviienko <matvienko@mettevo.org>
2023-07-09 22:16:58 +0300

    commit

--- src/components/pages/info/InfoMain/infoMainMobile/InfoMainMobile.jsx
+++ src/components/pages/info/InfoMain/infoMainMobile/InfoMainMobile.jsx
@@ -1,7 +1,9 @@
 import Avatar from "../../../../reusable/assets/avatar/Avatar"
 import CustomLink from "../../../../reusable/btns/links/CustomLink"
+import Rank from "../../../hub/items/rank/Rank"
 import InfoBio from "../../items/InfoBio"
 import InfoHeaderMobile from "./items/infoHeaderMobile/InfoHeaderMobile"
+import { Link } from "react-router-dom"
 
 const InfoMainMobile = () => {
   return (
@@ -15,6 +17,17 @@ const InfoMainMobile = () => {
             about me
           </CustomLink>
         </div>
+      </div>
+      <div className="mt-5">
+        <Rank />
+      </div>
+      <div className="mt-5 d-flex justify-content-between align-items-center">
+        <CustomLink to="/" className="alt-btn button_m">
+          Edit profile
+        </CustomLink>
+        <Link to="/" className="text-alt-primary info__edit-profile-m">
+          Profile preview
+        </Link>
       </div>
     </>
   )
---
+++ src/components/pages/info/items/infoDataBlocks/InfoBadges.jsx
@@ -0,0 +1,9 @@
+import Box from "../../../../reusable/cards/box/Box"
+
+const InfoBadgesTopComponent = () => <div></div>
+
+const InfoBadges = () => {
+  return <Box topComponent={<InfoBadgesTopComponent />}></Box>
+}
+
+export default InfoBadges
