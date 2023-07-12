ad385b88cca6234f2d56610950a0ee059810bd47
Alexandr Matviienko <matvienko@mettevo.org>
2023-07-12 12:13:43 +0300

    remove calendar from stats page

--- src/components/pages/hub/hubStats/HubStats.jsx
+++ src/components/pages/hub/hubStats/HubStats.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react"
+import { useState } from "react"
 import Rank from "../items/rank/Rank.jsx"
 import LinkTabs from "../../../reusable/assets/linkTabs/LinkTabs.jsx"
-import Calendar from "../../../reusable/calendar/Calendar.jsx"
 import FilterList from "../items/filterList/FilterList.jsx"
 import MainLayoutRightColumn from "../../../layouts/mainLayout/mainLayoutContent/mainLayoutRightColumn/MainLayoutRightColumn.jsx"
 import AdditionalInformation from "../../../reusable/assets/additionalInformation/AdditionalInformation.jsx"
@@ -224,7 +223,7 @@ const HubStats = () => {
           <LinkTabs list={tabsLast} />
           <UpdateInfo text="Points last synced on Today, 01:30 pm" onBtnClick={() => null} />
           <PeriodList items={periodList} onClick={(periodValue) => setPeriodType(periodValue)} value={periodType} />
-          <div className="box box_big mb-9">
+          {/* <div className="box box_big mb-9">
             <Calendar
               onChange={setCalendarValue}
               value={calendarValue}
@@ -232,7 +231,7 @@ const HubStats = () => {
               noIndent
               buttonProps={{ text: "See todays tasks", handler: () => null }}
             />
-          </div>
+          </div> */}
           <FilterList data={mockItems} />
         </div>
 
